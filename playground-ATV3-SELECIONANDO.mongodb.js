ef19cb9368b4094583e94e0ab49bb06155b31de3
Angelico4 <junior.bj2604@gmail.com>
2026-09-10 19:18:20 -0300

    ajustes

--- playground-ATV3-SELECIONANDO.mongodb.js
+++ playground-ATV3-SELECIONANDO.mongodb.js
@@ -1,25 +1,24 @@
-/*Criação da constante com o nome do banco de dados*/
 const database = 'BD3-NoSQL-Produtos';
-
-// habilita o banco para uso
 use(database);
 
-/* 
+/* MAIOR QUE 700
 db['bd3_nosql_atv7'].find({
 valor:{$gt:700}
 }) 
 */
 
-/* 
+/* MENOR QUE 450
 db['bd3_nosql_atv7'].find({
     valor:{$lt:450}
 }) 
 */
 
+/* ENTRE 500 E 950
 db['bd3_nosql_atv7'].find({
 $and:[
     {valor:{$gte:500}},
     {valor:{$lte:950}}
 ]
-})
+}) 
+*/
 
